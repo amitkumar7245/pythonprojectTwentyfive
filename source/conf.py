914fdbb4b9327440cb2ac7e5b43ef7a304661a0b
amitkumar7245 <amitkdna@gmail.com>
2025-07-22 21:26:15 +0530

    website the code update

--- source/conf.py
+++ source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'projectFive'
+project = 'Help Center'
 copyright = '2025, Author'
 author = 'Author'
 
